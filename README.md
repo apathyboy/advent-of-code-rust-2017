9dd39d83000d66d6299a530f10712b7d1fee3f56
Eric Barr <eb@eb2.co>
2024-01-27 01:17:26 -0700

    Day 12 solutions

--- README.md
+++ README.md
@@ -38,8 +38,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 9](./src/bin/09.rs) | `22.8µs` | `21.7µs` |
 | [Day 10](./src/bin/10.rs) | `1.8µs` | `162.6µs` |
 | [Day 11](./src/bin/11.rs) | `190.0µs` | `221.9µs` |
+| [Day 12](./src/bin/12.rs) | `365.7µs` | `30.1ms` |
 
-**Total: 279.36ms**
+**Total: 309.83ms**
 <!--- benchmarking table --->
 
 *Benchmarks performed on a 2021 M1 Macbook Pro, 16GB*
---
+++ src/bin/12.rs
@@ -0,0 +1,88 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+advent_of_code::solution!(12);
+
+fn parse_line(line: &str) -> Option<(u32, Vec<u32>)> {
+    let (program, neighbors) = line.split_once(" <-> ")?;
+
+    let program = program.parse::<u32>().ok()?;
+    let neighbors: Vec<u32> = neighbors
+        .split(", ")
+        .filter_map(|n| n.parse::<u32>().ok())
+        .collect();
+
+    Some((program, neighbors))
+}
+
+fn find_containing_group(pipes: &HashMap<u32, Vec<u32>>, val: u32) -> HashSet<u32> {
+    let mut nums = HashSet::new();
+    let mut to_visit = VecDeque::from([val]);
+
+    while !to_visit.is_empty() {
+        let next = to_visit.pop_front().unwrap();
+
+        nums.insert(next);
+
+        for neighbor in pipes.get(&next).unwrap() {
+            if !nums.contains(neighbor) {
+                to_visit.push_back(*neighbor);
+            }
+        }
+    }
+
+    nums
+}
+
+fn next_not_in_discovered(
+    pipes: &HashMap<u32, Vec<u32>>,
+    discovered_groups: &[HashSet<u32>],
+) -> Option<u32> {
+    pipes
+        .keys()
+        .find(|&key| !discovered_groups.iter().any(|v| v.contains(key)))
+        .cloned()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let pipes = input
+        .lines()
+        .filter_map(parse_line)
+        .collect::<HashMap<_, _>>();
+
+    let group = find_containing_group(&pipes, 0);
+
+    Some(group.len())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let pipes = input
+        .lines()
+        .filter_map(parse_line)
+        .collect::<HashMap<_, _>>();
+
+    let mut discovered_groups = Vec::new();
+
+    while let Some(program) = next_not_in_discovered(&pipes, &discovered_groups) {
+        let group = find_containing_group(&pipes, program);
+        discovered_groups.push(group);
+    }
+
+    Some(discovered_groups.len() as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result: Option<u32> = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+}
