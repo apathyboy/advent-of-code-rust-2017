979d1cf4e481b438f04a5047f3aa937ef26de93d
eb <eb@eb2.co>
2024-01-27 15:36:39 -0700

    Move knot hash to lib

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,70 +1,6 @@
+use advent_of_code::{knot_hash, to_hex_string, twist};
+
 advent_of_code::solution!(10);
-
-fn skip_and_reverse_wraparound(mut vec: Vec<usize>, n: usize, i: usize) -> Vec<usize> {
-    let len = vec.len();
-    if len == 0 || i == 0 {
-        return vec;
-    }
-
-    let start = n % len;
-    let end = (start + i) % len;
-
-    if start < end {
-        vec[start..start + i].reverse();
-    } else {
-        // Wrap around case
-        let mut temp = vec[start..].to_vec();
-        temp.extend_from_slice(&vec[..end]);
-        temp.reverse();
-        vec[start..].copy_from_slice(&temp[..len - start]);
-        vec[..end].copy_from_slice(&temp[len - start..]);
-    }
-
-    vec
-}
-
-fn twist(
-    lengths: &[usize],
-    mut list: Vec<usize>,
-    current_pos: &mut usize,
-    skip_length: &mut usize,
-) -> Vec<usize> {
-    for length in lengths {
-        list = skip_and_reverse_wraparound(list, *current_pos, *length);
-
-        *current_pos += *length + *skip_length;
-
-        *skip_length += 1;
-    }
-
-    list
-}
-
-fn to_hex(input: &[usize]) -> String {
-    input
-        .iter()
-        .fold(String::new(), |acc, &i| acc + &format!("{:02x}", i))
-}
-
-fn knot_hash(input: &str) -> Vec<usize> {
-    let lengths: Vec<usize> = input
-        .chars()
-        .map(|c| c as usize)
-        .chain(vec![17, 31, 73, 47, 23])
-        .collect();
-
-    let mut list = (0..256).collect::<Vec<_>>();
-    let mut current_pos = 0;
-    let mut skip_length = 0;
-
-    for _ in 0..64 {
-        list = twist(&lengths, list, &mut current_pos, &mut skip_length);
-    }
-
-    list.chunks(16)
-        .map(|chunk| chunk.iter().fold(0, |acc, &x| acc ^ x))
-        .collect()
-}
 
 pub fn part_one(input: &str) -> Option<usize> {
     let lengths: Vec<usize> = input
@@ -86,7 +22,7 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<String> {
-    Some(to_hex(&knot_hash(input.trim())))
+    Some(to_hex_string(&knot_hash(input.trim())))
 }
 
 #[cfg(test)]
@@ -116,6 +52,6 @@ mod tests {
     #[case("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
     fn test_knot_hash(#[case] input: &str, #[case] expected: &str) {
         let result = knot_hash(input);
-        assert_eq!(to_hex(&result), String::from(expected));
+        assert_eq!(to_hex_string(&result), String::from(expected));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,75 @@ pub mod template;
 pub fn parse_line_as_digits(line: &str) -> Vec<u32> {
     line.trim().chars().filter_map(|c| c.to_digit(10)).collect()
 }
+
+fn skip_and_reverse_wraparound(mut vec: Vec<usize>, n: usize, i: usize) -> Vec<usize> {
+    let len = vec.len();
+    if len == 0 || i == 0 {
+        return vec;
+    }
+
+    let start = n % len;
+    let end = (start + i) % len;
+
+    if start < end {
+        vec[start..start + i].reverse();
+    } else {
+        // Wrap around case
+        let mut temp = vec[start..].to_vec();
+        temp.extend_from_slice(&vec[..end]);
+        temp.reverse();
+        vec[start..].copy_from_slice(&temp[..len - start]);
+        vec[..end].copy_from_slice(&temp[len - start..]);
+    }
+
+    vec
+}
+
+pub fn twist(
+    lengths: &[usize],
+    mut list: Vec<usize>,
+    current_pos: &mut usize,
+    skip_length: &mut usize,
+) -> Vec<usize> {
+    for length in lengths {
+        list = skip_and_reverse_wraparound(list, *current_pos, *length);
+
+        *current_pos += *length + *skip_length;
+
+        *skip_length += 1;
+    }
+
+    list
+}
+
+pub fn to_hex_string(input: &[usize]) -> String {
+    input
+        .iter()
+        .fold(String::new(), |acc, &i| acc + &format!("{:02x}", i))
+}
+
+pub fn to_binary_string(input: &[usize]) -> String {
+    input
+        .iter()
+        .fold(String::new(), |acc, &i| acc + &format!("{:04b}", i))
+}
+
+pub fn knot_hash(input: &str) -> Vec<usize> {
+    let lengths: Vec<usize> = input
+        .chars()
+        .map(|c| c as usize)
+        .chain(vec![17, 31, 73, 47, 23])
+        .collect();
+
+    let mut list = (0..256).collect::<Vec<_>>();
+    let mut current_pos = 0;
+    let mut skip_length = 0;
+
+    for _ in 0..64 {
+        list = twist(&lengths, list, &mut current_pos, &mut skip_length);
+    }
+
+    list.chunks(16)
+        .map(|chunk| chunk.iter().fold(0, |acc, &x| acc ^ x))
+        .collect()
+}
