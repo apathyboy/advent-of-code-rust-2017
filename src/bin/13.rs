5a7223ecf0f11586055848b7f9f14efb4e161681
eb <eb@eb2.co>
2024-01-27 12:17:44 -0700

    Day 13 refactor

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -21,44 +21,14 @@ fn parse(input: &str) -> Vec<FirewallLayer> {
     input.lines().filter_map(parse_layer).collect()
 }
 
-fn cross_firewall(
-    layers: &[FirewallLayer],
-    max_layer_depth: usize,
-    start_time: usize,
-) -> (usize, bool) {
-    let mut severity = 0;
-    let mut detected = false;
-
-    for i in start_time..=max_layer_depth + start_time {
-        let layer = layers.iter().find(|layer| layer.depth == i - start_time);
-
-        if let Some(layer) = layer {
-            let sweeps = i / (layer.range - 1);
-            let mut scanner_pos = i.rem_euclid(layer.range - 1);
-
-            if sweeps.rem_euclid(2) == 1 {
-                scanner_pos = layer.range - scanner_pos - 1;
-            }
-
-            if scanner_pos == 0 {
-                severity += layer.depth * layer.range;
-                detected = true;
-            }
-        }
-    }
-
-    (severity, detected)
-}
-
 pub fn part_one(input: &str) -> Option<usize> {
     let layers = parse(input);
 
-    let max_layer_depth = layers
+    let severity = layers
         .iter()
-        .map(|layer| layer.depth)
-        .max_by(|&a, &b| a.cmp(&b))?;
-
-    let (severity, _) = cross_firewall(&layers, max_layer_depth, 0);
+        .filter(|&layer| layer.depth.rem_euclid(2 * (layer.range - 1)) == 0)
+        .map(|layer| layer.depth * layer.range)
+        .sum();
 
     Some(severity)
 }
@@ -66,20 +36,13 @@ pub fn part_one(input: &str) -> Option<usize> {
 pub fn part_two(input: &str) -> Option<usize> {
     let layers = parse(input);
 
-    let max_layer_depth = layers
-        .iter()
-        .map(|layer| layer.depth)
-        .max_by(|&a, &b| a.cmp(&b))?;
-
-    for i in 0.. {
-        let (_, detected) = cross_firewall(&layers, max_layer_depth, i);
-
-        if !detected {
-            return Some(i);
-        }
-    }
-
-    None
+    (0..)
+        .filter(|delay| {
+            !layers
+                .iter()
+                .any(|layer| (delay + layer.depth).rem_euclid(2 * (layer.range - 1)) == 0)
+        })
+        .next()
 }
 
 #[cfg(test)]
